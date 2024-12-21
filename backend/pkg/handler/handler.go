2fe53ed58d85d43f3bb1b3a4685539032edf0c44
yaakovlew <andrei252002@mail.ru>
2024-12-21 15:47:04 +0300

    upd [cors]: err

--- backend/pkg/handler/handler.go
+++ backend/pkg/handler/handler.go
@@ -40,7 +40,7 @@ func (h *Controller) InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://mephi22.undersite.ru"},
+		AllowOrigins:     []string{"https://mephi22.undersite.ru", "https://mephi71.ru"},
 		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
