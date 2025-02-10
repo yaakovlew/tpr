53596acfd7e15e1ec8a324645af5780c59b438a0
Ruslan <Ruslan@DESKTOP-0RC4LOK>
2025-02-10 14:04:29 +0300

    change main page text

--- frontend/src/pages/main/MainPage.vue
+++ frontend/src/pages/main/MainPage.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="main flex items-center column q-ma-xs-md">
-    <h4>Новости</h4>
+    <h4>Добро пожаловать</h4>
+    <h6 class="text-center">
+      Добро пожаловать в систему поддержки обучения по дисциплинам, входящим в блок «Теория принятия решения и мягкие вычисления».
+      <br>
+      Система доступна для авторизированных пользователей – нажмите на иконку в правом верхнем углу, укажите номер группы и аккуратно введите Вашу фамилию и имя.
+    </h6>
     <div class="main__news-container flex items-center column">
       <single-news-container title="Лекции и семинары">
         <template #content>
           <div>
-            Видеозаписи лекций и семинаров прошлых лет находятся по адресу
+            Видеозаписи лекций и семинаров прошлых лет находятся по <a href="https://disk.yandex.ru/d/cQQGC05bWGPoMA">адресу</a>
           </div>
         </template>
       </single-news-container>
